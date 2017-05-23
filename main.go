ebbe581e962a309891caa66538d3387fc847260c
Brett Demetris <brett.demetris@airbnb.com>
2017-05-23 15:06:01 -0700

    now have the keys and values from the inbound csv

--- main.go
+++ main.go
@@ -4,18 +4,51 @@ import (
   "os/exec"
   "fmt"
   "log"
+  "bufio"
   "strings"
 )
 
 func main () {
-  echo := exec.Command("wmic", "bios", "list", "full")
+  cmd := exec.Command("wmic", "bios", "list", "full", "/format:csv")
 
-  b, err := echo.Output()
+  o, err := cmd.Output()
   if err != nil {
       log.Fatal(err)
   }
 
-  s := string(b)
+  s := string(o)
 
-  fmt.Println((strings.Split(s, "\r"))[1])
+  var csv_lines []string
+
+  scanner := bufio.NewScanner(strings.NewReader(s))
+  for scanner.Scan() {
+    csv_lines = append(csv_lines, scanner.Text())
+  }
+
+  keys := csv_lines[1]
+  values := csv_lines[2]
+
+  fmt.Println(keys + "\n")
+  fmt.Println(values)
+
+
+  // r := csv.NewReader(strings.NewReader(s))
+  // fmt.Println(r)
+
+  // for {
+  //   record, err := r.ReadAll()
+  //   if err == io.EOF {
+  //     break
+  //   }
+  //   if err != nil {
+  //     log.Fatal(err)
+  //     fmt.Println(record)
+  //   }
+
+  //   fmt.Println(record)
+  // }
+
+
+
+  // fmt.Println(s)
 }
