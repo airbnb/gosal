e2bf086b2fb928e44e83b0aca0931d726fbdf1d7
Brett Demetris <brett.demetris@airbnb.com>
2017-06-08 09:32:21 -0700

    kind of working disk space struct

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 
 func main () {
 
-  wb, _ := Get_win32_bios()
+  test, _ := Get_win32_logicaldisk()
 
-  fmt.Printf("%+v\n", wb)
+  fmt.Printf("%#v", test)
+
 }
 
 func Get_win32_bios() (win32_bios, error) {
@@ -30,7 +31,54 @@ func Get_win32_bios() (win32_bios, error) {
   return j, nil
 }
 
+func Get_win32_os() (win32_os, error) {
+  cmd := exec.Command("powershell", "gwmi", "Win32_OperatingSystem", "|", "convertto-json")
+
+  o, err := cmd.Output()
+  if err != nil {
+    return win32_os{}, err
+  }
+
+  var j win32_os
+
+  if err := json.Unmarshal(o, &j); err != nil{
+    return win32_os{}, err
+  }
+
+  return j, nil
+}
+
+func Get_win32_logicaldisk() ([]win32_logicaldisk, error) {
+  cmd := exec.Command("powershell", "Get-WmiObject", "Win32_LogicalDisk", "|", "convertto-json")
+
+  o, err := cmd.Output()
+  if err != nil {
+    return nil, err
+  }
+
+  var j []win32_logicaldisk
+
+  if err := json.Unmarshal(o, &j); err != nil{
+    return nil, err
+  }
+
+  return j, nil
+
+}
+
+type win32_logicaldisk struct {
+	Name string `json:"Name"`
+	Size int64 `json:"Size"`
+	FreeSpace int64 `json:"Free"`
+}
+
 type win32_bios struct {
-	PSComputerName string  `json:"PSComputerName"`
-	SerialNumber   string  `json:"SerialNumber"`
+	PSComputerName          string   `json:"PSComputerName"`
+	SerialNumber            string   `json:"SerialNumber"`
+}
+
+type win32_os struct {
+  Caption                 string    `json:"Caption"`
+  TotalVirtualMemorySize  string    `json:"TotalVirtualMemorySize"`
+  TotalVisibleMemorySize  string    `json:"TotalVisibleMemorySize"`
 }
