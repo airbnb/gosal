fd54ea859dc3664767f661a2492034f57c4cccb7
Brett Demetris <bdemetris@gmail.com>
2017-07-11 14:29:13 -0700

    first crack at mapping json objects together

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
   "github.com/bdemetris/gosal/reports"
   "fmt"
+  "encoding/json"
 )
 
 func main () {
 
-  test, _ := reports.Get_win32_logicaldisk()
+  win32_os, _ := reports.Get_win32_os()
+  win32_bios, _ := reports.Get_win32_bios()
+  win32_logicaldisk, _ := reports.Get_win32_logicaldisk()
 
-  fmt.Printf("%#v", test)
+  var report map[string]string
+
+  json_win32_os, _ := json.Marshal(win32_os)
+  json.Unmarshal(json_win32_os, &report)
+
+  json_win32_bios, _ := json.Marshal(win32_bios)
+  json.Unmarshal(json_win32_bios, &report)
+
+  json_win32_logicaldisk, _ := json.Marshal(win32_logicaldisk)
+  json.Unmarshal(json_win32_logicaldisk, &report)
+
+  json_report, _ := json.Marshal(report)
+  fmt.Println(string(json_report))
+  // test, _ := reports.Get_win32_logicaldisk()
+  //
+  // fmt.Printf("%#v", test)
 }
