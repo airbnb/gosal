d0bc109f5be0197988edad1429bb901ecf1c1a4d
Brett Demetris <brett.demetris@airbnb.com>
2017-09-02 16:47:43 -0700

    sends disk information from c drive specifically

--- reports/win32_logicaldisk.go
+++ reports/win32_logicaldisk.go
@@ -27,5 +27,23 @@ func GetWin32LogicalDisk() ([]Win32LogicalDisk, error) {
 type Win32LogicalDisk struct {
 	Name      string `json:"Name"`
 	Size      int    `json:"Size"`
-	FreeSpace int64  `json:"Free"`
+	FreeSpace int    `json:"Free"`
+}
+
+func GetCDrive() (Win32LogicalDisk, error) {
+
+  disks, _ := GetWin32LogicalDisk()
+
+	var c Win32LogicalDisk
+
+	for _, element := range disks {
+		if element.Name == "C:" {
+			c.Name = element.Name
+			c.Size = element.Size
+			c.FreeSpace = element.FreeSpace
+			// var f := element.FreeSpace
+		}
+	}
+
+	return c, nil
 }
