65230db4b58411c33200c7bbc4ee50861895cae1
Brett Demetris <bdemetris@gmail.com>
2017-09-03 14:56:20 -0700

    Limit scope to machine info

--- reports/emulator_machineinfo.go
+++ reports/emulator_machineinfo.go
@@ -4,18 +4,12 @@ import (
 	"log"
 )
 
-func EmulateManagedInstallReport() (ManagedInstallReport, error) {
+func EmulateMachineInfo() (MachineInfo, error) {
 
 	win32Bios, err := GetWin32Bios()
 	if err != nil {
 		// TODO return the error here?
 		log.Printf("reports: getting win32 bios: %s", err)
-	}
-
-	cDrive, err := GetCDrive()
-	if err != nil {
-		// TODO return the error here?
-		log.Printf("reports: getting win32 disk: %s", err)
 	}
 
 	win32ComputerSystem, err := GetWin32ComputerSystem()
@@ -30,27 +24,15 @@ func EmulateManagedInstallReport() (ManagedInstallReport, error) {
 		log.Printf("reports: getting win32 os: %s", err)
 	}
 
-	report := ManagedInstallReport{
-		AvailableDiskSpace: cDrive.FreeSpace,
-		ConsoleUser:        win32ComputerSystem.UserName,
-		MachineInfo: []MachineInfo{
-			{
+	report := MachineInfo{
 				Hostname:     win32Bios.PSComputerName,
 				MachineModel: win32ComputerSystem.Model,
 				OSVersion:    win32OS.Caption,
 				SerialNumber: win32Bios.SerialNumber,
-			},
-		},
 	}
 
-	return report, nil
+	return &report, nil
 
-}
-
-type ManagedInstallReport struct {
-	AvailableDiskSpace int
-	ConsoleUser        string
-	MachineInfo        []MachineInfo
 }
 
 type MachineInfo struct {
