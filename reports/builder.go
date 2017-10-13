c19546dbf993c6c36b760378696ce7a4a7ed15aa
Brett Demetris <bdemetris@gmail.com>
2017-10-13 14:05:04 -0700

    Implement error handling in builder.go

--- reports/builder.go
+++ reports/builder.go
@@ -1,28 +1,26 @@
 package reports
 
 import (
-	"log"
 	"strconv"
 
+	"github.com/pkg/errors"
 	"github.com/satori/go.uuid"
 )
 
 // BuildReport builds the report object
-func BuildReport(apiKey string) Report {
+func BuildReport(apiKey string) (*Report, error) {
 
 	win32Bios, _ := GetWin32Bios()
 	CDrive, err := GetCDrive()
 	if err != nil {
-		// TODO return the error here?
-		log.Printf("reports: getting win32 disk: %s", err)
+		return nil, errors.Wrap(err, "reports: getting win32 disk")
 	}
 
 	u1 := uuid.NewV4().String()
 
 	encodedCompressedPlist, err := BuildBase64bz2Report()
 	if err != nil {
-		// TODO return the error here?
-		log.Printf("reports: getting plist: %s", err)
+		return nil, errors.Wrap(err, "reports: getting plist")
 	}
 
 	report := Report{
@@ -36,7 +34,7 @@ func BuildReport(apiKey string) Report {
 	}
 
 	// fmt.Printf("%+v\n", report)
-	return report
+	return &report, nil
 }
 
 // Report structure
