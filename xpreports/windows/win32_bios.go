d4b18c2a83015e563615d47956736c7e2c3f786f
Gavin Elder <gavin@improbable.io>
2019-08-03 19:06:58 +0100

    Clean Commit.

--- xpreports/windows/win32_bios.go
+++ xpreports/windows/win32_bios.go
@@ -31,19 +31,3 @@ type Win32Bios struct {
 	PSComputerName string `json:"PSComputerName"`
 	SerialNumber   string `json:"SerialNumber"`
 }
-
-/*
-owershell gwmi Win32_Bios |ConvertTo-Json
-[
-    "",
-    "",
-    "SMBIOSBIOSVersion : VMW71.00V.12343141.B64.1902160724",
-    "Manufacturer      : VMware, Inc.",
-    "Name              : VMW71.00V.12343141.B64.1902160724",
-    "SerialNumber      : VMware-56 4d ea bd d1 c3 fe 6d-80 7a ba 09 22 3f ba 82",
-    "Version           : INTEL  - 6040000",
-    "",
-    "",
-    ""
-]
-*/
