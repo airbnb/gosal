13b941ee58d9c0326eb3485574faa48a275380ec
Gavin Elder <gavin@improbable.io>
2019-11-30 21:15:17 +0000

    Fix failing build.

--- xpreports/build_linux.go
+++ xpreports/build_linux.go
@@ -6,15 +6,12 @@ import (
 	"github.com/airbnb/gosal/config"
 	"github.com/airbnb/gosal/xpreports/linux"
 	"github.com/pkg/errors"
-	uuid "github.com/satori/go.uuid"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
 )
 
 // buildReport creates a report using linux APIs and paths.
 func buildMachineReport(conf *config.Config) (*Machine, error) {
-	u1 := uuid.NewV4().String()
-
 	host, err := host.Info()
 	if err != nil {
 		return nil, errors.Wrap(err, "reports: getting host information")
@@ -39,7 +36,6 @@ func buildMachineReport(conf *config.Config) (*Machine, error) {
 	consoleUser, _ := linux.GetLoggedInUsers()
 
 	v, _ := mem.VirtualMemory()
-	h, _ := host.Info()
 
 	computerSystem, _ := linux.GetLinuxComputerSystem()
 
@@ -70,7 +66,7 @@ func buildMachineReport(conf *config.Config) (*Machine, error) {
 			HostName:             host.Hostname,
 			ConsoleUser:          consoleUser[0],
 			OSFamily:             "Linux",
-			OperatingSystem:      h.PlatformVersion,
+			OperatingSystem:      host.PlatformVersion,
 			HDSpace:              disk.FreeSpace,
 			HDTotal:              disk.Size,
 			MachineModel:         computerSystem.Model,
