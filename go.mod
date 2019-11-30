06c229e756590e7da4fb239a8df2989af9241764
Gavin Elder <gavin@improbable.io>
2019-11-30 18:32:43 +0000

    Tidy up go mods

--- go.mod
+++ go.mod
@@ -1,7 +1,15 @@
 module github.com/airbnb/gosal
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/dselans/dmidecode v0.0.0-20180814053009-65c3f9d81910
+	github.com/dsnet/compress v0.0.1
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/groob/plist v0.0.0-20190114192801-a99fbe489d03
 	github.com/pkg/errors v0.8.1
+	github.com/satori/go.uuid v1.2.0
+	github.com/shirou/gopsutil v2.19.10+incompatible
+	github.com/shirou/w32 v0.0.0-20160930032740-bb4de0191aa4 // indirect
 	github.com/spf13/cobra v0.0.5
 )
 
---
+++ xpreports/common/users.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func GetLoggedInUsers() ([]string, error) {
+	cmd := exec.Command("who", "-us")
+	users, _ := cmd.CombinedOutput()
+
+	cleaned := []string{}
+
+	for _, user := range strings.Split(string(users), "\n") {
+		clean := true
+		col := strings.Split(user, " ")
+
+		if len(col) > 0 {
+			for _, cleanedU := range cleaned {
+				u := strings.TrimSpace(col[0])
+				if len(u) == 0 || strings.Compare(cleanedU, col[0]) == 0 {
+					clean = false
+				}
+			}
+			if clean {
+				cleaned = append(cleaned, col[0])
+			}
+		}
+	}
+
+	return cleaned, nil
+}
